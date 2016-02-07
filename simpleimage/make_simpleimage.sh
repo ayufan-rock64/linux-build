aff225ca618c546dac514030922d1af50f1fc698
Simon Eisenmann <simon@longsleep.org>
2016-02-07 17:52:11 +0100

    Cleaned up and improved.

--- simpleimage/make_simpleimage.sh
+++ simpleimage/make_simpleimage.sh
@@ -2,21 +2,24 @@
 #
 # Simple script to create a disk image which boots to U-Boot on Pine64.
 #
-# This scripts used boot0 binary blob extracted from the Pine64 Android image
-# together with a compiled U-Boot from the BSP.
+# This scripts uses boot0 binary blob (as extracted from the Pine64 Android
+# image) together with a correctly prefixed U-Boot, and A DOS partition table
+# to create a bootable SDcard image for the Pine64.
 #
 # U-Boot trees:
 # - https://github.com/longsleep/u-boot-pine64/tree/lichee-dev-2014.07-fix-compile
 #
-# Build the U-Boot tree, patch the u-boot with a fex file and and put the
+# Build the U-Boot tree, patch the U-boot with a fex file and and put the
 # created u-boot.fex into the same directory as this script. Also extract the
-# boot0 binary blob from the Android image as released by Pine64.
+# boot0 binary blob from the Android image as released by Pine64 and put it
+# into the same directory.
 #
-# Extract boot0 from image:
+# This is how to extract boot0 from an existing image:
 #
 # ```bash
 # dd if="$IMAGE" bs=1k skip=8 count=32 of=boot0-android.bin
 # ```
+#
 
 set -e
 
@@ -26,10 +29,12 @@ uboot="./u-boot-with-dtb.bin"
 boot0_position=8
 uboot_position=19096
 part_position=20480
+disk_size=100 # MiB
+boot_size=50  # MiB
 
-out="./simpleimage-bsp.img"
+out="./simpleimage.img"
 
-dd if=/dev/zero bs=1M count=100 of="$out"
+dd if=/dev/zero bs=1M count=$disk_size of="$out"
 dd if="$boot0" conv=notrunc bs=1k seek=$boot0_position of="$out"
 dd if="$uboot" conv=notrunc bs=1k seek=$uboot_position of="$out"
 
@@ -37,9 +42,8 @@ dd if="$uboot" conv=notrunc bs=1k seek=$uboot_position of="$out"
 cat <<EOF | sfdisk -f "$out"
 label: dos
 unit: sectors
-
-img1 : start=       40960, size=      102401, type=c
-img2 : start=      143361, size=       61439, type=83
+start=$((part_position*2)), size=${boot_size}M, type=c
+start=$((part_position*2 + boot_size*1024*2)), type=83
 EOF
 
 # Create file systems
