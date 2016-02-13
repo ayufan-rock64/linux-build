ca5df067320faaf9363554a2cb46f1b982b05cfb
Simon Eisenmann <simon@longsleep.org>
2016-02-13 16:36:03 +0100

    Added support bo boot an actualy rootfs.

--- kernel/make_initrd.sh
+++ kernel/make_initrd.sh
@@ -4,8 +4,13 @@
 # busybox static binary. You can also use any other initrd for example one
 # from Debian like # https://d-i.debian.org/daily-images/arm64/20160206-00:06/netboot/debian-installer/arm64/
 #
+# Run this script with fakeroot or as root.
 
 set -e
+
+if [ "$(id -u)" -ne "0" ]; then
+	exec fakeroot $0 $@
+fi
 
 BUSYBOX="../busybox"
 
@@ -20,10 +25,31 @@ mkdir dev proc sys tmp sbin
 mknod dev/console c 5 1
 cat > $TEMP/init <<EOF
 #!/bin/busybox sh
+
+# Install busybox
 /bin/busybox --install -s
+
+# Mount the /proc and /sys filesystems.
 mount -t proc none /proc
 mount -t sysfs none /sys
 mount -t devtmpfs none /dev
+
+if [ -e /dev/mmcblk0p2 ]; then
+	# Mount real root.
+	mkdir -p /mnt/root
+	mount -o ro /dev/mmcblk0p2 /mnt/root
+
+	# Cleanup.
+	umount /proc
+	umount /sys
+	umount /dev
+
+	# Boot the real system.
+	exec switch_root /mnt/root /sbin/init
+fi
+
+echo "Failed to enter system - dropping to shell!"
+echo
 exec /bin/sh
 EOF
 chmod 755 $TEMP/init
