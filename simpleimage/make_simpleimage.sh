486c08a9b060baa6dad68c9f3900569b7b8a5aa7
Simon Eisenmann <simon@longsleep.org>
2016-02-21 13:11:06 +0100

    Create partition table as very last step to speed up image creation. Use disk labels BOOT and rootfs. Optimize ext4 settings for SD cards.

--- simpleimage/make_simpleimage.sh
+++ simpleimage/make_simpleimage.sh
@@ -47,9 +47,29 @@ boot_size=50          # MiB
 
 set -x
 
-dd if=/dev/zero bs=1M count=$disk_size of="$out"
+# Create beginning of disk
+dd if=/dev/zero bs=1M count=$((part_position/1024)) of="$out"
 dd if="$boot0" conv=notrunc bs=1k seek=$boot0_position of="$out"
 dd if="$uboot" conv=notrunc bs=1k seek=$uboot_position of="$out"
+
+# Create boot file system (VFAT)
+dd if=/dev/zero bs=1M count=${boot_size} of=${out}1
+mkfs.vfat -n BOOT ${out}1
+
+# Add boot support if there
+if [ -e "${kernel}/kernel.img" -a -e "${kernel}/sun50i-a64-pine64-plus.dtb" ]; then
+	mcopy -i ${out}1 ${kernel}/kernel.img ::
+	mcopy -i ${out}1 ${kernel}/*.dtb ::
+	mcopy -i ${out}1 ${kernel}/uEnv.txt :: || true
+fi
+dd if=${out}1 conv=notrunc oflag=append bs=1M seek=$((part_position/1024)) of="$out"
+rm -f ${out}1
+
+# Create additional ext4 file system for rootfs
+dd if=/dev/zero bs=1M count=$((disk_size-boot_size-part_position/1024)) of=${out}2
+mkfs.ext4 -b 4096 -E stride=2,stripe-width=1024 -L rootfs ${out}2
+dd if=${out}2 conv=notrunc oflag=append bs=1M seek=$((part_position/1024+boot_size)) of="$out"
+rm -f ${out}2
 
 # Add partition table
 cat <<EOF | sfdisk -q -f "$out"
@@ -58,24 +78,6 @@ unit: sectors
 start=$((part_position*2)), size=${boot_size}M, type=c
 start=$((part_position*2 + boot_size*1024*2)), type=83
 EOF
-
-# Create boot file system (VFAT)
-dd if=/dev/zero bs=1M count=${boot_size} of=${out}1
-mkfs.vfat ${out}1
-# Add boot stuff if there.
-if [ -e "${kernel}/kernel.img" -a -e "${kernel}/sun50i-a64-pine64-plus.dtb" ]; then
-	mcopy -i ${out}1 ${kernel}/kernel.img ::
-	mcopy -i ${out}1 ${kernel}/*.dtb ::
-	mcopy -i ${out}1 ${kernel}/uEnv.txt ::
-fi
-dd if=${out}1 conv=notrunc bs=1k seek=${part_position} of="$out"
-rm -f ${out}1
-
-# Create additional ext4 file system.
-dd if=/dev/zero bs=1M count=$((disk_size-boot_size-part_position/1024)) of=${out}2
-mkfs.ext4 ${out}2
-dd if=${out}2 conv=notrunc bs=1M seek=$((part_position/1024+boot_size)) of="$out"
-rm -f ${out}2
 
 sync
 
