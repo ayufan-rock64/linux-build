58cfe109966ab7678b56f3533bdb0de9138c1b20
Simon Eisenmann <simon@longsleep.org>
2016-02-28 16:54:24 +0100

    Added helper to install kernel modules easily.

---
+++ kernel/install_kernel_modules.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e
+
+DEST="$1"
+
+if [ -z "$DEST" ]; then
+	echo "Usage: $0 <destination-folder> [linux-folder]"
+	exit 1
+fi
+
+LINUX="../linux"
+
+if [ -n "$2" ]; then
+	LINUX="$2"
+fi
+
+echo "Using Linux from $LINUX ..."
+
+# Install Kernel modules
+make -C $LINUX ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- modules_install INSTALL_MOD_PATH="$DEST"
+
+# Install extra mali module if found in Kernel tree.
+if [ -e $LINUX/modules/gpu/mali400/kernel_mode/driver/src/devicedrv/mali/mali.ko ]; then
+	v=$(ls $DEST/lib/modules/)
+	mkdir "$DEST/lib/modules/$v/kernel/extramodules"
+	cp -v $LINUX/modules/gpu/mali400/kernel_mode/driver/src/devicedrv/mali/mali.ko $DEST/lib/modules/$v/kernel/extramodules
+	depmod -b $DEST $v
+fi
+
+echo "Done - installed Kernel modules to $DEST"
