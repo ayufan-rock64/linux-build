5be285ccf76889180a88473423409ba83660b752
Simon Eisenmann <simon@longsleep.org>
2016-03-05 16:08:28 +0100

    Add proper /etc/hosts for Xenial.

--- simpleimage/make_rootfs.sh
+++ simpleimage/make_rootfs.sh
@@ -120,6 +120,17 @@ EOF
 auto eth0
 iface eth0 inet dhcp
 EOF
+		cat > "$DEST/etc/hosts" <<EOF
+127.0.0.1 localhost
+127.0.1.1 localhost.localdomain
+
+# The following lines are desirable for IPv6 capable hosts
+::1     localhost ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+EOF
 		sed -i 's|After=rc.local.service|#\0|;' "$DEST/lib/systemd/system/serial-getty@.service"
 		rm -f "$DEST/second-phase"
 		rm -f "$DEST/etc/resolv.conf"
