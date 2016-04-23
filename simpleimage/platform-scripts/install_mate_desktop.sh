ee7ad86c04dc4a33e5f1a2d4fb26d5acb1df1246
Simon Eisenmann <simon@longsleep.org>
2016-04-23 22:23:09 +0200

    Add checks for Debian and root.

--- simpleimage/platform-scripts/install_mate_desktop.sh
+++ simpleimage/platform-scripts/install_mate_desktop.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+set -e
+
+if ! hash apt-get 2>/dev/null; then
+	echo "This script requires a Debian based distribution."
+	exit 1
+fi
+
+if [ "$(id -u)" -ne "0" ]; then
+	echo "This script requires root."
+	exit 1
+fi
+
 apt-get -y update
 apt-get -y --no-install-recommends install \
 	xserver-xorg-video-fbturbo \
