925a7b91b80f726d8bdf5cbe23b17d6ce39797df
Simon Eisenmann <simon@longsleep.org>
2016-05-01 22:43:23 +0200

    Use default audio.

--- simpleimage/platform-scripts/mplayer-play.sh
+++ simpleimage/platform-scripts/mplayer-play.sh
@@ -21,4 +21,4 @@ if [ ! -w /dev/disp ]; then
 fi
 
 export VDPAU_DRIVER=sunxi
-exec mplayer -ao alsa:device=hw=1.0 -vo vdpau -vc ffmpeg12vdpau,ffh264vdpau,ffhevcvdpau, $@
+exec mplayer -vo vdpau -vc ffmpeg12vdpau,ffh264vdpau,ffhevcvdpau, $@
