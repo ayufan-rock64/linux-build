46c4572e59b8ef76b714b48c8b0d01ebfc727970
Andrea Scarpino <me@andreascarpino.it>
2016-11-19 11:51:35 +0100

    arch: update system before installing new packages
    Arch Linux does not support partial updates. If you sync your
    repositories you are supposed to do a full update before installing new
    packages otherwise you could get binaries built with different
    libraries.

--- simpleimage/make_rootfs.sh
+++ simpleimage/make_rootfs.sh
@@ -288,8 +288,9 @@ case $DISTRO in
 		mv "$DEST/etc/resolv.conf" "$DEST/etc/resolv.conf.dist"
 		cp /etc/resolv.conf "$DEST/etc/resolv.conf"
 		sed -i 's|CheckSpace|#CheckSpace|' "$DEST/etc/pacman.conf"
+		do_chroot pacman -Syu --noconfirm || true
 		do_chroot pacman -Rsn --noconfirm linux-aarch64 || true
-		do_chroot pacman -Sy --noconfirm --needed dosfstools curl xz iw rfkill netctl dialog wpa_supplicant alsa-utils || true
+		do_chroot pacman -S --noconfirm --needed dosfstools curl xz iw rfkill netctl dialog wpa_supplicant alsa-utils || true
 		add_platform_scripts
 		add_mackeeper_service
 		add_corekeeper_service
