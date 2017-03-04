416b24423b530650b1cd57782d2923ed1bed9111
Jacob Chen <jacob-chen@iotwrt.com>
2017-03-04 10:54:57 +0800

    uboot: lower case chip name
    Signed-off-by: Jacob Chen <jacob-chen@iotwrt.com>
    
    # Conflicts:
    #	mk-uboot.sh

--- mk-uboot.sh
+++ mk-uboot.sh
@@ -35,16 +35,16 @@ echo Using ${UBOOT_DEFCONFIG}
 cd ${LOCALPATH}/u-boot
 make ${UBOOT_DEFCONFIG} all
 
-if [ "${CHIP}" == "RK3288" ] ; then
+if [ "${CHIP}" == "rk3288" ] ; then
 	tools/mkimage -n rk3288 -T \
 		 rksd -d spl/u-boot-spl-dtb.bin u-boot.out
 	cat u-boot-dtb.bin >> u-boot.out
 	cp u-boot.out ${OUT}/u-boot/
-elif [ "${CHIP}" == "RK3036" ] ; then
+elif [ "${CHIP}" == "rk3036" ] ; then
 	tools/mkimage -n rk3036 -T rksd -d spl/u-boot-spl.bin u-boot.out
 	cat u-boot-dtb.bin >> u-boot.out
-	cp  uboot.out ${OUT}/u-boot/
-elif [ "${CHIP}" == "RK3399" ] ; then
+	cp  u-boot.out ${OUT}/u-boot/
+elif [ "${CHIP}" == "rk3399" ] ; then
 	loaderimage --pack --uboot ./u-boot-dtb.bin uboot.img
 
 	cd ../
@@ -53,7 +53,7 @@ elif [ "${CHIP}" == "RK3399" ] ; then
 
 	cp  uboot.img ${OUT}/u-boot/
 	mv  ../trust.img ${OUT}/u-boot/
-elif [ "${CHIP}" == "RK3328" ] ; then
+elif [ "${CHIP}" == "rk3328" ] ; then
 	loaderimage --pack --uboot ./u-boot-dtb.bin uboot.img
 	cd ../
 	$TOOLPATH/trust_merger $TOOLPATH/RK3328TRUST.ini
