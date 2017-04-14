49196387939d95f4171ec0abbf67ae33ed2dc5e5
Jacob Chen <jacob-chen@iotwrt.com>
2017-04-14 15:28:28 +0800

    uboot: remove rk3399
    not ready
    
    Signed-off-by: Jacob Chen <jacob-chen@iotwrt.com>

--- mk-uboot.sh
+++ mk-uboot.sh
@@ -44,15 +44,6 @@ elif [ "${CHIP}" == "rk3036" ] ; then
 	tools/mkimage -n rk3036 -T rksd -d spl/u-boot-spl.bin idbloader.img
 	cat u-boot-dtb.bin >> idbloader.img
 	cp  idbloader.img ${OUT}/u-boot/
-elif [ "${CHIP}" == "rk3399" ] ; then
-	loaderimage --pack --uboot ./u-boot-dtb.bin uboot.img
-
-	cd ../
-	$TOOLPATH/trust_merger $TOOLPATH/RK3399TRUST.ini
-	cd ${LOCALPATH}/u-boot
-
-	cp  uboot.img ${OUT}/u-boot/
-	mv  ../trust.img ${OUT}/u-boot/
 elif [ "${CHIP}" == "rk3328" ] ; then
 	loaderimage --pack --uboot ./u-boot-dtb.bin uboot.img
 
