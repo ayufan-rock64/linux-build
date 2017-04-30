6e574631579126b21fe935d75a3bb1d4c3946b92
Simon Eisenmann <simon@longsleep.org>
2017-04-30 15:45:28 +0200

    Add additional optional parameters for model and bl31 release type

--- u-boot-postprocess/u-boot-postprocess.sh
+++ u-boot-postprocess/u-boot-postprocess.sh
@@ -15,20 +15,32 @@ BLOBS="../blobs"
 UBOOT="../u-boot-pine64"
 # https://github.com/longsleep/arm-trusted-firmware-pine64
 TRUSTED_FIRMWARE="../arm-trusted-firmware-pine64"
-TRUSTED_FIRMWARE_BUILD="release"
 # https://github.com/longsleep/sunxi-pack-tools
 SUNXI_PACK_TOOLS="../sunxi-pack-tools/bin"
+
+if [ "$1" = "-h" -o "$1" = "--help" ]; then
+    echo "Usage: $0 [<pine64|pinebook>] [<trusted-firmware-build>]"
+    exit 1
+fi
+MODEL="$1"
+
+TRUSTED_FIRMWARE_BUILD="$2"
+if [ -z "$TRUSTED_FIRMWARE_BUILD" ]; then
+    TRUSTED_FIRMWARE_BUILD="release"
+fi
 
 BUILD="../build"
 mkdir -p $BUILD
 
+echo "ATF build: $TRUSTED_FIRMWARE_BUILD"
 cp -avf $TRUSTED_FIRMWARE/build/sun50iw1p1/$TRUSTED_FIRMWARE_BUILD/bl31.bin $BUILD
 cp -avf $UBOOT/u-boot-sun50iw1p1.bin $BUILD/u-boot.bin
 cp -avf $BLOBS/scp.bin $BUILD
 cp -avf $BLOBS/sys_config.fex $BUILD
 
 # build binary device tree
-dtc -Odtb -o $BUILD/pine64.dtb $BLOBS/pine64.dts
+echo "Device tree model: $MODEL"
+dtc -Odtb -o $BUILD/$MODEL.dtb $BLOBS/$MODEL.dts
 
 unix2dos $BUILD/sys_config.fex
 $SUNXI_PACK_TOOLS/script $BUILD/sys_config.fex
@@ -38,10 +50,9 @@ $SUNXI_PACK_TOOLS/merge_uboot $BUILD/u-boot.bin $BUILD/bl31.bin $BUILD/u-boot-me
 $SUNXI_PACK_TOOLS/merge_uboot $BUILD/u-boot-merged.bin $BUILD/scp.bin $BUILD/u-boot-merged2.bin scp
 
 # update_fdt.exe u-boot.bin xxx.dtb output_file.bin
-$SUNXI_PACK_TOOLS/update_uboot_fdt $BUILD/u-boot-merged2.bin $BUILD/pine64.dtb $BUILD/u-boot-with-dtb.bin
+$SUNXI_PACK_TOOLS/update_uboot_fdt $BUILD/u-boot-merged2.bin $BUILD/$MODEL.dtb $BUILD/u-boot-with-dtb.bin
 
 # Add fex file to u-boot so it actually is accepted by boot0.
 $SUNXI_PACK_TOOLS/update_uboot $BUILD/u-boot-with-dtb.bin $BUILD/sys_config.bin
 
 echo "Done - created $BUILD/u-boot-with-dtb.bin"
-
