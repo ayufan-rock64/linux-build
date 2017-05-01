24514ec28a57d72c829548b97dbbf2e62eb68276
Simon Eisenmann <simon@longsleep.org>
2017-05-01 22:27:26 +0200

    Use slim instead of lightdm

--- simpleimage/platform-scripts/install_minimal_desktop.sh
+++ simpleimage/platform-scripts/install_minimal_desktop.sh
@@ -24,39 +24,45 @@ case $DISTRO in
 		apt-get -y --no-install-recommends install \
 			xserver-xorg-video-fbturbo \
 			xserver-xorg-input-all \
-			lightdm \
-			lightdm-gtk-greeter \
-            xvt \
+			slim \
+			xvt \
 			openbox
 		;;
 	*)
 		;;
 esac
 
+if [ ! -d /usr/share/slim/themes/pine64 ]; then
+	cp -ra /usr/share/slim/themes/default /usr/share/slim/themes/pine64
+	wget -O /usr/share/slim/themes/pine64/background.png \
+		https://github.com/longsleep/build-pine64-image/raw/master/bootlogo/bootlogo-pine64-1366x768.png
+	sed s/^current_theme(.*)/current_theme pine64/g /etc/slim.conf
+fi
+
 mkdir -p /etc/X11/xorg.conf.d
 
 # Make X11 use fbturbo driver.
 cat > "/etc/X11/xorg.conf.d/40-pine64-fbturbo.conf" <<EOF
 Section "Device"
-        Identifier      "Allwinner A10/A13 FBDEV"
-        Driver          "fbturbo"
-        Option          "fbdev" "/dev/fb0"
+		Identifier      "Allwinner A10/A13 FBDEV"
+		Driver          "fbturbo"
+		Option          "fbdev" "/dev/fb0"
 
-        Option          "SwapbuffersWait" "true"
+		Option          "SwapbuffersWait" "true"
 EndSection
 EOF
 
 # Add configuration for Pinebook touchpad so it is usable.
 cat > "/etc/X11/xorg.conf.d/50-pine64-pinebook-touchpad.conf" <<EOF
 Section "InputClass"
-   Identifier "HAILUCK CO.,LTD USB KEYBOARD"
-   MatchIsPointer "1"
-   MatchDevicePath "/dev/input/event*"
+	Identifier "HAILUCK CO.,LTD USB KEYBOARD"
+	MatchIsPointer "1"
+	MatchDevicePath "/dev/input/event*"
 
-   Option "AccelerationProfile" "2"
-   Option "AdaptiveDeceleration" "1"
-   Option "ConstantDeceleration" "2.4" # Pinebook 14"
-   #Option "ConstantDeceleration" "1.2" # Pinebook 11"
+	Option "AccelerationProfile" "2"
+	Option "AdaptiveDeceleration" "1"
+	Option "ConstantDeceleration" "2.4" # Pinebook 14"
+	#Option "ConstantDeceleration" "1.2" # Pinebook 11"
 EndSection
 EOF
 
