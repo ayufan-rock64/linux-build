797f7e14661c7a632c32c44141e41f7ca1a5475e
Simon Eisenmann <simon@longsleep.org>
2017-05-01 22:26:54 +0200

    Use tab indentation

--- simpleimage/platform-scripts/install_mate_desktop.sh
+++ simpleimage/platform-scripts/install_mate_desktop.sh
@@ -56,25 +56,25 @@ mkdir -p /etc/X11/xorg.conf.d
 # Make X11 use fbturbo driver.
 cat > "/etc/X11/xorg.conf.d/40-pine64-fbturbo.conf" <<EOF
 Section "Device"
-        Identifier      "Allwinner A10/A13 FBDEV"
-        Driver          "fbturbo"
-        Option          "fbdev" "/dev/fb0"
+		Identifier      "Allwinner A10/A13 FBDEV"
+		Driver          "fbturbo"
+		Option          "fbdev" "/dev/fb0"
 
-        Option          "SwapbuffersWait" "true"
+		Option          "SwapbuffersWait" "true"
 EndSection
 EOF
 
 # Add configuration for Pinebook touchpad so it is usable.
 cat > "/etc/X11/xorg.conf.d/50-pine64-pinebook-touchpad.conf" <<EOF
 Section "InputClass"
-   Identifier "HAILUCK CO.,LTD USB KEYBOARD"
-   MatchIsPointer "1"
-   MatchDevicePath "/dev/input/event*"
+	Identifier "HAILUCK CO.,LTD USB KEYBOARD"
+	MatchIsPointer "1"
+	MatchDevicePath "/dev/input/event*"
 
-   Option "AccelerationProfile" "2"
-   Option "AdaptiveDeceleration" "1"
-   Option "ConstantDeceleration" "2.4" # Pinebook 14"
-   #Option "ConstantDeceleration" "1.2" # Pinebook 11"
+	Option "AccelerationProfile" "2"
+	Option "AdaptiveDeceleration" "1"
+	Option "ConstantDeceleration" "2.4" # Pinebook 14"
+	#Option "ConstantDeceleration" "1.2" # Pinebook 11"
 EndSection
 EOF
 
