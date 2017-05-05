691c37c79d8c35dbc88a5df86017c746fc213010
whongx <whongx6789@gmail.com>
2017-05-05 18:12:08 +0800

    Preinstall apps and add pinebook background image files

--- package/root/usr/local/sbin/pinebook_change_background.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -xe
-
-#Add pinebook wallpaper
-echo "\nChange Pinebook wallpaper..."
-gsettings set org.mate.background picture-filename /usr/share/backgrounds/ubuntu-mate-pinebook/Pinebook-Wallpaper-6.jpg
-echo "\nChanged!!!\n"
--- package/root/usr/local/sbin/pinebook_preinstall_apps.sh
+++ package/root/usr/local/sbin/pinebook_preinstall_apps.sh
@@ -3,25 +3,10 @@
 set -xe
 
 #Install relevant libraries and applications
-echo "\nInstalling programs...\n"
 add-apt-repository -y ppa:rvm/smplayer
+add-apt-repository -y ppa:longsleep/ubuntu-pine64-flavour-makers
 apt-get -y update
-apt-get -y install aisleriot build-essential emacs gcc geany gimp git gnomine gnome-sudoku htop libpixman-1-dev libvdpau-dev mplayer scratch smplayer smplayer-skins smplayer-themes smtube vim
-echo "\nInstalled!!!\n"
+apt-get -y install aisleriot build-essential emacs gcc geany gimp git gnomine gnome-sudoku htop libpixman-1-dev libvdpau-dev libvdpau-sunxi1 mplayer scratch smplayer smplayer-skins smplayer-themes smtube vim
 
-
-#Solve video issue of cannot stream video smoothly
-echo "\nFix Pinebook video...\n"
-git clone https://github.com/linux-sunxi/libcedrus.git
-make -C libcedrus install 
-git clone https://github.com/linux-sunxi/libvdpau-sunxi.git
-make -C libvdpau-sunxi install
-ln -s /usr/lib/aarch64-linux-gnu/vdpau/libvdpau_sunxi.so.1 /usr/lib/libvdpau_nvidia.so
-rm -rf libcedrus libvdpau-sunxi
-echo "\nPinebook video is fixed!!!\n"
-
-
-#Remove scripts that may cause the system crash
-#rm -rf /usr/local/sbin/pine64*
 apt-get -y upgrade
 apt-get -y update
