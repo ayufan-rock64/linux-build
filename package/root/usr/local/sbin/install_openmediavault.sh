251b6fa34f930487e7a140420048c6d405f1afdc
Kamil Trzcinski <ayufan@ayufan.eu>
2017-06-24 23:26:16 +0200

    Use github packages for OpenMediaVault

--- package/root/usr/local/sbin/install_openmediavault.sh
+++ package/root/usr/local/sbin/install_openmediavault.sh
@@ -11,11 +11,11 @@ set -xe
 
 #Add OMV source.list and Update System
 cat > /etc/apt/sources.list.d/openmediavault.list <<- EOF
-deb http://packages.openmediavault.org/public erasmus main
-# deb https://openmediavault.github.io/packages/ erasmus main
+# deb http://packages.openmediavault.org/public erasmus main
+deb https://openmediavault.github.io/packages/ erasmus main
 ## Uncomment the following line to add software from the proposed repository.
-deb http://packages.openmediavault.org/public erasmus-proposed main
-# deb https://openmediavault.github.io/packages/ erasmus-proposed main
+# deb http://packages.openmediavault.org/public erasmus-proposed main
+deb https://openmediavault.github.io/packages/ erasmus-proposed main
 
 ## This software is not part of OpenMediaVault, but is offered by third-party
 ## developers as a service to OpenMediaVault users.
