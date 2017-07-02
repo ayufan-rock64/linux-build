036fab1ea1ef08a4abef749ba6b82009c570c753
Kamil Trzcinski <ayufan@ayufan.eu>
2017-07-02 16:00:35 +0200

    Add copyright

--- package/root/usr/local/sbin/rock64_fix_irqs.sh
+++ package/root/usr/local/sbin/rock64_fix_irqs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Taken from: https://forum.armbian.com/index.php?/topic/3953-preview-generate-omv-images-for-sbc-with-armbian/#comment-34596
+
 echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 for i in 1 2 3 ; do
 	echo 4 >/proc/irq/$(awk -F":" "/xhci/ {print \$1}" </proc/interrupts | sed 's/\ //g')/smp_affinity
