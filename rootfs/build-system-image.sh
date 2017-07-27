95bea4d9f250e2c7801dc3457f778bc6f32e73b1
Kamil Trzciński <ayufan@ayufan.eu>
2017-07-27 13:44:55 +0200

    Update build-system-image.sh

--- rootfs/build-system-image.sh
+++ rootfs/build-system-image.sh
@@ -75,7 +75,8 @@ dd if=/dev/zero of="$TEMP/$IMAGE" bs=1M seek=$SIZE count=0
 dd if=/dev/zero of="$TEMP/$BOOT_IMAGE" bs=1M seek=$BOOT_SIZE count=0
 
 # Make filesystem
-mkfs.ext4 -L "linux-root" "$TEMP/$IMAGE" && tune2fs -o journal_data_writeback "$TEMP/$IMAGE"
+mkfs.ext4 -L "linux-root" "$TEMP/$IMAGE"
+tune2fs -o journal_data_writeback "$TEMP/$IMAGE"
 mkfs.vfat -n "boot" -S 512 "$TEMP/$BOOT_IMAGE"
 
 # Mount filesystem
