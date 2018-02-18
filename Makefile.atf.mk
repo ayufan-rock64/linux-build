7521524372ce761692cea6d091427206da50e460
Kamil Trzciński <ayufan@ayufan.eu>
2018-02-18 12:13:40 +0100

    atf: fix compilation for rk3399

--- Makefile.atf.mk
+++ Makefile.atf.mk
@@ -2,11 +2,11 @@ BL31 ?= arm-trusted-firmware/build/$(ATF_PLAT)/release/bl31/bl31.elf
 
 arm-trusted-firmware/build/$(ATF_PLAT)/release/bl31/bl31.elf: arm-trusted-firmware
 	make -C $< realclean
-	make -C $< CROSS_COMPILE=aarch64-linux-gnu- PLAT=$(ATF_PLAT) bl31
+	make -C $< CROSS_COMPILE=aarch64-linux-gnu- M0_CROSS_COMPILE=arm-linux-gnueabi- PLAT=$(ATF_PLAT) bl31
 
 arm-trusted-firmware/build/$(ATF_PLAT)/debug/bl31/bl31.elf: arm-trusted-firmware
 	make -C $< realclean
-	make -C $< CROSS_COMPILE=aarch64-linux-gnu- PLAT=$(ATF_PLAT) bl31 DEBUG=1
+	make -C $< CROSS_COMPILE=aarch64-linux-gnu- M0_CROSS_COMPILE=arm-linux-gnueabi- PLAT=$(ATF_PLAT) bl31 DEBUG=1
 
 .PHONY: atf-build
 atf-build: $(BL31)
