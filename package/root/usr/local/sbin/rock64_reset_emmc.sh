bf286c71a4db7e3b3d1db448f892885d16eca045
Kamil Trzciński <ayufan@ayufan.eu>
2018-03-31 23:29:59 +0200

    Added reset-emmc script to easily deal with jumper

---
+++ package/root/usr/local/sbin/rock64_reset_emmc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ "$1" != "--force" ]]; the
+    MNT_DEV=$(findmnt / -n -o SOURCE)
+    if [[ "$MNT_DEV" == /dev/mmcblk0* ]]; then
+        echo "Cannot reset when running from eMMC, use: $0 --force."
+        exit 1
+    fi
+fi
+
+if [[ -d /sys/bus/platform/drivers/dwmmc_rockchip/ff520000.dwmmc ]]; then
+    echo "Unbinding..."
+    echo ff520000.dwmmc > /sys/bus/platform/drivers/dwmmc_rockchip/unbind
+fi
+
+echo "Binding..."
+echo ff520000.dwmmc > /sys/bus/platform/drivers/dwmmc_rockchip/bind
+
+echo "Finished"
