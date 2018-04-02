1b8320304a301dcedbc49efea7c97ee1fde79c8d
Kamil Trzciński <ayufan@ayufan.eu>
2018-04-02 10:53:16 +0200

    Fix reset emmc script

--- package/root/usr/local/sbin/rock64_reset_emmc.sh
+++ package/root/usr/local/sbin/rock64_reset_emmc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$1" != "--force" ]]; the
+if [[ "$1" != "--force" ]]; then
     MNT_DEV=$(findmnt / -n -o SOURCE)
     if [[ "$MNT_DEV" == /dev/mmcblk0* ]]; then
         echo "Cannot reset when running from eMMC, use: $0 --force."
