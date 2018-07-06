4920d3eaa65e0ff178868d7e9cc3d5967d0cbd8d
Kamil Trzciński <ayufan@ayufan.eu>
2018-07-06 14:55:15 +0200

    Preserve environment variables

--- Makefile.system.mk
+++ Makefile.system.mk
@@ -5,7 +5,7 @@
 	pxz -f -3 $<
 
 %.img: $(PACKAGES)
-	sudo bash rootfs/build-system-image.sh \
+	sudo -E bash rootfs/build-system-image.sh \
 		"$$(readlink -f $@)" \
 		"$(filter $(BUILD_SYSTEMS), $(subst -, ,$(*F)))" \
 		"$(filter $(BUILD_VARIANTS), $(subst -, ,$(*F)))" \
