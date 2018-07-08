2a5482d4b1a4b962f2c81e65094e6c232f5f1ddb
Kamil Trzciński <ayufan@ayufan.eu>
2018-07-08 11:48:05 +0200

    Optimise variants handling

--- Makefile.variants.mk
+++ Makefile.variants.mk
@@ -1,42 +1,13 @@
-.PHONY: stretch-minimal
-stretch-minimal: stretch-minimal-$(BOARD_TARGET)-$(IMAGE_SUFFIX)-arm64.img.xz
+VARIANTS := $(foreach system, $(BUILD_SYSTEMS), $(foreach variants, $(BUILD_VARIANTS), $(system)-$(variants)))
 
-.PHONY: jessie-openmediavault
-stretch-openmediavault: stretch-openmediavault-$(BOARD_TARGET)-$(IMAGE_SUFFIX)-armhf.img.xz \
-	stretch-openmediavault-$(BOARD_TARGET)-$(IMAGE_SUFFIX)-arm64.img.xz
+variants:
+	@echo $(VARIANTS)
 
-.PHONY: bionic-minimal
-bionic-minimal: bionic-minimal-$(BOARD_TARGET)-$(IMAGE_SUFFIX)-armhf.img.xz \
-	bionic-minimal-$(BOARD_TARGET)-$(IMAGE_SUFFIX)-arm64.img.xz
-
-.PHONY: bionic-containers
-bionic-containers: bionic-containers-$(BOARD_TARGET)-$(IMAGE_SUFFIX)-arm64.img.xz
-
-.PHONY: bionic-mate
-bionic-mate: bionic-mate-$(BOARD_TARGET)-$(IMAGE_SUFFIX)-arm64.img.xz
-
-.PHONY: bionic-lxde
-bionic-lxde: bionic-lxde-$(BOARD_TARGET)-$(IMAGE_SUFFIX)-arm64.img.xz
-
-.PHONY: bionic		# build all bionic variants
-bionic: bionic-minimal \
-	bionic-containers \
-	bionic-lxde
-
-.PHONY: stretch		# build all stretch variants
-stretch: stretch-minimal \
-	stretch-openmediavault
+$(addsuffix -armhf, $(VARIANTS)): %-armhf: %-$(BOARD_TARGET)-$(IMAGE_SUFFIX)-armhf.img.xz
+$(addsuffix -arm64, $(VARIANTS)): %-arm64: %-$(BOARD_TARGET)-$(IMAGE_SUFFIX)-arm64.img.xz
 
 .PHONY: linux-variants		# build all linux variants
 linux-variants: \
-	bionic \
-	stretch \
-	linux-virtual
-
-.PHONY: linux-minimal		# build all linux variants
-linux-minimal: \
-	bionic-minimal \
-	bionic-containers \
-	stretch-minimal \
-	stretch-openmediavault \
+	bionic-minimal-arm64 \
+	stretch-minimal-arm64 \
 	linux-virtual
