2f7bd6097303eb35de79fffb81a26c6e82e2fc9a
Kamil Trzciński <ayufan@ayufan.eu>
2019-10-08 14:35:45 +0100

    Add `$DEBUSER` to `lpadmin` group
    This solves the ability to modify CUPS

--- rootfs/make_rootfs.sh
+++ rootfs/make_rootfs.sh
@@ -253,7 +253,7 @@ done
 if [[ "$DEBUSER" != "root" ]]; then
 	do_chroot adduser --gecos "$DEBUSER" --disabled-login "$DEBUSER" --uid 1000
 	do_chroot chown -R 1000:1000 "/home/$DEBUSER"
-	do_chroot usermod -a -G sudo,audio,adm,input,video,plugdev,ssh "$DEBUSER"
+	do_chroot usermod -a -G sudo,audio,adm,input,video,plugdev,ssh,lp,lpadmin "$DEBUSER"
 fi
 
 # Change and expire password
