ad31c4ff2b2b8d05b91f842d04ec4a04a6c12197
Kamil Trzciński <ayufan@ayufan.eu>
2020-04-07 17:47:00 +0200

    Use better names for partitions

--- rootfs/build-system-image.sh
+++ rootfs/build-system-image.sh
@@ -94,33 +94,43 @@ LODEVMAPPER="${LODEV/\/dev\/loop/\/dev\/mapper\/loop}"
 # Assign partitions
 kpartx -a "$LODEV"
 
+LODEV_UBOOT="${LODEVMAPPER}p1"
+LODEV_EFI="${LODEVMAPPER}p2"
+LODEV_BOOT="${LODEVMAPPER}p3"
+LODEV_ROOT="${LODEVMAPPER}p4"
+
 # Make filesystem
-mkfs.vfat -n "boot-efi" -S 512 "${LODEVMAPPER}p2"
-mkfs.ext4 -L "linux-boot" "${LODEVMAPPER}p3"
-mkfs.ext4 -L "linux-root" "${LODEVMAPPER}p4"
-tune2fs -o journal_data_writeback "${LODEVMAPPER}p4"
+mkfs.vfat -n "boot-efi" -S 512 "${LODEV_EFI}"
+mkfs.ext4 -L "linux-boot" "${LODEV_BOOT}"
+mkfs.ext4 -L "linux-root" "${LODEV_ROOT}"
+tune2fs -o journal_data_writeback "${LODEV_ROOT}"
 
 # Mount filesystem
 mkdir -p "$TEMP/rootfs"
-mount -o data=writeback,commit=3600 "${LODEVMAPPER}p4" "$TEMP/rootfs"
+mount -o data=writeback,commit=3600 "${LODEV_ROOT}" "$TEMP/rootfs"
 mkdir -p "$TEMP/rootfs/boot"
-mount "${LODEVMAPPER}p3" "$TEMP/rootfs/boot"
+mount "${LODEV_BOOT}" "$TEMP/rootfs/boot"
 mkdir -p "$TEMP/rootfs/boot/efi"
-mount "${LODEVMAPPER}p2" "$TEMP/rootfs/boot/efi"
+mount "${LODEV_EFI}" "$TEMP/rootfs/boot/efi"
 
 # Create image
 unshare -m -u -i -p --mount-proc --fork -- \
     rootfs/make-rootfs.sh "$TEMP/rootfs" "$DISTRO" "$VARIANT" "$BUILD_ARCH" "$MODEL" "$@"
 
 # Write bootloader
-dd if="$TEMP/rootfs/usr/lib/u-boot-${MODEL}/rksd_loader.img" of="${LODEVMAPPER}p1"
+dd if="$TEMP/rootfs/usr/lib/u-boot-${MODEL}/rksd_loader.img" of="${LODEV_UBOOT}"
 
-# Umount filesystem
+# Sync all filesystems
 sync -f "$TEMP/rootfs" "$TEMP/rootfs/boot" "$TEMP/rootfs/boot/efi"
 fstrim "$TEMP/rootfs"
 fstrim "$TEMP/rootfs/boot"
 df -h "$TEMP/rootfs" "$TEMP/rootfs/boot" "$TEMP/rootfs/boot/efi"
 mv "$TEMP/rootfs/all-packages.txt" "$(dirname "$OUT_IMAGE")/$(basename "$OUT_IMAGE" .img)-packages.txt"
+
+# Umount filesystems
+umount "$TEMP/rootfs/boot/efi"
+umount "$TEMP/rootfs/boot"
+umount "$TEMP/rootfs"
 
 # Cleanup build
 cleanup
